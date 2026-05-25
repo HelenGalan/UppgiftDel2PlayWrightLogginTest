50c95d0f5633263e22c0510acccab88d82ae171f
HelenGalan <helen.galan@gmail.com>
2026-05-25 16:27:57 +0200

    Creating Project

---
+++ src/main/java/com/example/UppgiftDel2PlayWrightLogginTest/UppgiftDel2PlayWrightLogginTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.UppgiftDel2PlayWrightLogginTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UppgiftDel2PlayWrightLogginTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UppgiftDel2PlayWrightLogginTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UppgiftDel2PlayWrightLogginTest/UppgiftDel2PlayWrightLogginTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UppgiftDel2PlayWrightLogginTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UppgiftDel2PlayWrightLogginTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
