78f85dddb0d17623689b553d15ce92c44498b3e9
HelenGalan <helen.galan@gmail.com>
2026-05-25 17:10:48 +0200

    Create the first test step

--- src/test/java/com/example/UppgiftDel2PlayWrightLogginTest/UppgiftDel2PlayWrightLogginTestApplicationTests.java
+++ src/test/java/com/example/UppgiftDel2PlayWrightLogginTest/UppgiftDel2PlayWrightLogginTestApplicationTests.java
@@ -2,12 +2,18 @@ package com.example.UppgiftDel2PlayWrightLogginTest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.microsoft.playwright.*;
 
 @SpringBootTest
 class UppgiftDel2PlayWrightLogginTestApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testLoginPage() {
+		try (Playwright playwright = Playwright.create()) {
+			Browser browser = playwright.chromium().launch();
+			Page page = browser.newPage();
+			page.navigate("https://www.saucedemo.com/");
+			System.out.println(page.title());
+		}
 	}
-
 }
