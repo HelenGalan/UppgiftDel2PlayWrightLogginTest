f6e462c78e3b4822012ecdb2df6df15c912b7682
HelenGalan <helen.galan@gmail.com>
2026-05-25 17:22:13 +0200

    Create step fill in and assertions

--- src/test/java/com/example/UppgiftDel2PlayWrightLogginTest/UppgiftDel2PlayWrightLogginTestApplicationTests.java
+++ src/test/java/com/example/UppgiftDel2PlayWrightLogginTest/UppgiftDel2PlayWrightLogginTestApplicationTests.java
@@ -3,6 +3,9 @@ package com.example.UppgiftDel2PlayWrightLogginTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.LoadState;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest
 class UppgiftDel2PlayWrightLogginTestApplicationTests {
@@ -12,8 +15,20 @@ class UppgiftDel2PlayWrightLogginTestApplicationTests {
 		try (Playwright playwright = Playwright.create()) {
 			Browser browser = playwright.chromium().launch();
 			Page page = browser.newPage();
+
 			page.navigate("https://www.saucedemo.com/");
-			System.out.println(page.title());
+
+			page.fill("#user-name", "standard_user");
+			page.fill("#password", "secret_sauce");
+
+			page.click("#login-button");
+
+			page.waitForLoadState(LoadState.NETWORKIDLE);
+
+			assertTrue(page.url().contains("inventory.html"),
+					"Should be in the page after the login");
+
+			assertEquals("Swag Labs", page.title());
 		}
 	}
 }
